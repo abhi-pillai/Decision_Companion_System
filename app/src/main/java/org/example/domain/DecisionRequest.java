82f0746d7f2f1e42465d8c001e4db5b0af865acc
abhi-pillai <12a.abhishekpillai@gmail.com>
2026-02-23 23:53:01 +0530

    feat(domain): Added public DecisionRequest() {}

--- app/src/main/java/org/example/domain/DecisionRequest.java
+++ app/src/main/java/org/example/domain/DecisionRequest.java
@@ -3,19 +3,39 @@ package org.example.domain;
 import java.util.List;
 
 public class DecisionRequest {
+    private String category;
     private List<Option> options;
     private List<Criterion> criteria;
 
-    public DecisionRequest(List<Option> options, List<Criterion> criteria) {
+    public DecisionRequest() {}
+
+    public DecisionRequest(String category, List<Option> options, List<Criterion> criteria) {
+        this.category = category;
         this.options = options;
         this.criteria = criteria;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
     }
 
     public List<Option> getOptions() {
         return options;
     }
 
+    public void setOptions(List<Option> options) {
+        this.options = options;
+    }
+
     public List<Criterion> getCriteria() {
         return criteria;
+    }
+
+    public void setCriteria(List<Criterion> criteria) {
+        this.criteria = criteria;
     }
 }
