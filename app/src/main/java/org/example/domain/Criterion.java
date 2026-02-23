ce32b18312d2ee2f8e49145ceac6f7adc1bfffaf
abhi-pillai <12a.abhishekpillai@gmail.com>
2026-02-23 23:51:03 +0530

    feat(domain):Added public Criterion() {} (no-arg constructor) and setName(), setWeight()

--- app/src/main/java/org/example/domain/Criterion.java
+++ app/src/main/java/org/example/domain/Criterion.java
@@ -4,6 +4,8 @@ public class Criterion {
     private String name;
     private double weight;
 
+    public Criterion() {}
+
     public Criterion(String name, double weight) {
         this.name = name;
         this.weight = weight;
@@ -13,7 +15,15 @@ public class Criterion {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public double getWeight() {
         return weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
     }
 }
